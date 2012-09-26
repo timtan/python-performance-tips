78d5f942150ed91a05aeccad4d726d7de48136e3
tim hsu <tim.yellow@gmail.com>
2012-09-27 06:32:43 +0800

    add sample for profile sample

---
+++ profile_sample1.py
@@ -0,0 +1,25 @@
+import re
+
+def find_domain(target_url):
+    pattern = re.compile("https?://([^/]+)/")
+    match = pattern.match(target_url)
+    if match :
+        return match.group(1)
+    return ""
+
+def main():
+    target_url = "https://www.google.com/search?q=Enhancement&aq=f&sugexp=chrome,mod=0&sourceid=chrome&ie=UTF-8"
+    counter    = {}
+
+    for i in range(900000):
+        domain = find_domain(target_url)  ## It will find www.google.com
+        try:
+            counter[domain] +=1
+        except KeyError:
+            counter[domain] = 0
+    print "finish"
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+__author__ = 'tim'
+from setuptools import  setup, find_packages
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+#ext_modules = [Extension(sources=['profile_sample1.py'], name='profile_sample1')]
+
+setup(
+    name = 'performance_samples',
+    install_requires = [
+        'Cython',
+        ],
+    version = 1.0,
+    description = "samples about performance",
+    author = 'wen chang',
+    author_email = 'tim.yellow@gmailc.om',
+    packages = find_packages(),
+    py_modules= ['profile_sample1', 'profile_sample2'],
+    entry_points = {
+        'console_scripts': [],
+        },
+    package_data = {},
+    cmdclass = {'build_ext': build_ext },
+    ext_modules = ext_modules,
+)
